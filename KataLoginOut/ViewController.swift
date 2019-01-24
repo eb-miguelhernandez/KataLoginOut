4fc7b9a6242cc72b0053d039f257e54920e3d4dd
Miguel Hernández Jaso <miguel.hernandez@eventbrite.com>
2019-01-24 15:50:21 +0100

    Initial Commit

---
+++ KataLoginOut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KataLoginOut
+//
+//  Created by Miguel Hernández Jaso on 24/01/2019.
+//  Copyright © 2019 Miguel Hernández Jaso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
