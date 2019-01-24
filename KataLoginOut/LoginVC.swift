915cc321b025e02d2ea798f32b3d358109be50f7
Miguel Hernández Jaso <miguel.hernandez@eventbrite.com>
2019-01-24 17:09:02 +0100

    Add logout logic

--- KataLoginOut/LoginVC.swift
+++ KataLoginOut/LoginVC.swift
@@ -12,6 +12,24 @@ class LoginVC: UIViewController {
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var login: UIButton!
+    @IBOutlet weak var loginPanel: UIView! {
+        didSet {
+            loginPanel.isHidden = false
+        }
+    }
+    @IBOutlet weak var logout: UIButton!
+    @IBOutlet weak var logoutPanel: UIView!{
+        didSet {
+            loginPanel.isHidden = true
+        }
+    }
+
+    // MARK: Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.showScene(.login)
+    }
 
     // MARK: Actions
 
@@ -22,6 +40,7 @@ class LoginVC: UIViewController {
         case .success(let result):
             guard let resultMessage = result as? String else { assertionFailure("Result must be a string"); return }
             message = resultMessage
+            self.showScene(.logout)
         case .failure(let error):
             message = error
         }
@@ -32,8 +51,44 @@ class LoginVC: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
 
-    // MARK: Business logic
+    @IBAction func logoutWasTapped(_ sender: UIButton) {
+        let result = self.logout(withDate: Date())
+        var message = ""
+        switch result {
+        case .success(let result):
+            guard let resultMessage = result as? String else { assertionFailure("Result must be a string"); return }
+            message = resultMessage
+            self.showScene(.login)
+        case .failure(let error):
+            message = error
+        }
 
+        let alert = UIAlertController(title: "Result", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    // MARK: View Logic
+
+    private func showScene(_ scene: Scene) {
+        switch scene {
+        case .login:
+            self.loginPanel.isHidden = false
+            self.logoutPanel.isHidden = true
+        case .logout:
+            self.loginPanel.isHidden = true
+            self.logoutPanel.isHidden = false
+        }
+    }
+
+    enum Scene {
+        case login
+        case logout
+    }
+}
+
+struct Login {
     func login(withUser user: String?, password: String?) -> Result {
         if user == "admin" && password == "admin" {
             return Result.success(result: "OK")
@@ -41,8 +96,19 @@ class LoginVC: UIViewController {
         return Result.failure(error: "A tomar por culo")
     }
 
-    enum Result {
-        case success(result: Any)
-        case failure(error: String)
+    func logout(withDate date: Date) -> Result {
+        if self.millisencondsFromDate(date) % 2 == 0 {
+            return Result.success(result: "OK")
+        }
+        return Result.failure(error: "No te puedes escapar")
     }
+
+    func millisencondsFromDate(_ date: Date) -> Int {
+        return Int((date.timeIntervalSince1970 * 1000.0).rounded())
+    }
+}
+
+enum Result {
+    case success(result: Any)
+    case failure(error: String)
 }
